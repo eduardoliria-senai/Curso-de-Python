489d0ee7a45c140a92761545c4f6d3ec8956da4c
eduardoliria-senai <eduardo.liria@docente.senai.br>
2026-08-26 21:46:00 -0300

    Aula 26/08

--- 07 - projeto/projeto.py
+++ 07 - projeto/projeto.py
@@ -22,4 +22,101 @@ def carregar_produtos():
 
     return produtos
 
-print(carregar_produtos())
+# funcao para mostrar produtos 
+def mostrar_produtos(produtos):
+
+    # titulo
+    print("\n ===== PRODUTOS =====")
+
+    for produto in produtos:
+        print(
+            f'{produto['produto']} | '
+            f'{produto['categoria']} | '
+            f'R$ {produto['preco']:.2} | '
+            f'Estoque {produto['quantidade']} | '
+            f'{produto['fornecedor']}'
+        )
+
+# Funcao para calcular a media
+def calcular_media(produtos): 
+    soma = 0
+
+    for produto in produtos:
+        soma += produto['preco']
+
+    media = soma / len(produtos)
+
+    print(f"\n A media de preços é de: R$ {media:.2f}")
+
+
+# funcao para filtrar valores acima de algum valor
+def produtos_acima(produtos):
+
+    valor = float(input("\nMostrar produtos acima de R$: "))
+    encontrados = 0
+
+    for produto in produtos:
+
+        if produto['preco'] > valor:
+            print(
+                f'{produto['produto']} - '
+                f'R$ {produto['preco']:.2f} '
+            )
+
+            encontrados += 1
+
+    if encontrados == 0:
+        print("Nenhum produto encontrado.")
+
+    print(f"Quantidade de produtos encontrados foi de: {encontrados}")
+
+# funcao para filtrar valores abaixo de algum valor
+def produtos_abaixo(produtos):
+    valor = float(input('\nMostrar produtos abaixo de R$'))
+    encontrados = 0
+
+    for produto in produtos:
+        if produto['preco'] < valor:
+            print(
+                f'{produto['produto']} - '
+                f'R$ {produto['preco']:.2f} '
+            )
+            encontrados += 1
+
+    if encontrados == 0:
+        print('Nenhum produto encontrado')
+
+    print(f"Quantidade de produtos encontrados foi de: {encontrados}")
+        
+
+# funcao para pegar o produto mais caro
+def produto_mais_caro(produtos):
+    mais_caro = produtos[0]
+
+    for produto in produtos:
+        if produto['preco'] > mais_caro['preco']:
+            mais_caro = produto
+
+    print("\n==== PRODUTO MAIS CARO É ====\n")
+
+    print(f'Produto: {mais_caro['produto']}')
+    print(f'Preco: {mais_caro['preco']:.2f}')
+    print(f'Categoria: {mais_caro['categoria']:.2f}')
+
+
+produtos = carregar_produtos()
+
+while True:
+
+    opcao = int(input('Digite uma opcao: '))
+
+    if opcao == 1:
+        mostrar_produtos(produtos)
+    elif opcao == 2:
+        calcular_media(produtos)
+    elif opcao == 3:
+        produtos_acima(produtos)
+    elif opcao == 4:
+        produtos_abaixo(produtos)
+    elif opcao == 5:
+        break
