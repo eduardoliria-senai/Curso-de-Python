fce09a5d93b543aa6d4dbb38369ae96b4c19c794
eduardoliria-senai <eduardo.liria@docente.senai.br>
2026-08-27 21:45:02 -0300

    Aula 27/08

--- 07 - projeto/projeto.py
+++ 07 - projeto/projeto.py
@@ -48,7 +48,6 @@ def calcular_media(produtos):
 
     print(f"\n A media de preços é de: R$ {media:.2f}")
 
-
 # funcao para filtrar valores acima de algum valor
 def produtos_acima(produtos):
 
@@ -89,6 +88,53 @@ def produtos_abaixo(produtos):
     print(f"Quantidade de produtos encontrados foi de: {encontrados}")
         
 
+# filtrar por CATEGORIA 
+def filtrar_categoria(produtos):
+    # mostrar todas as categorias
+    print('\n === Todas as categorias ==== \n')
+    for produto in produtos:
+        print(f'Categoria: {produto['categoria']}')
+
+    categoria = input("Digite a categoria: ")
+    encontrados = 0
+
+    print(f'\n ==== CATEGORIA: {categoria} ====')
+
+    for produto in produtos:
+        if produto['categoria'].lower() == categoria.lower():
+            print(
+                f'{produto['produto']} - '
+                f'{produto['preco']:.2f}'
+            )
+
+            encontrados = encontrados + 1
+
+    if encontrados == 0:
+        print('Nenhum produto encontrado')
+
+# filtra por fornecedor
+def filtra_fornecedor(produtos):
+    # mostrar todas as fornecedores
+    print('\n === Todas os Fornecedores ==== \n')
+    encontrados = 0
+    for produto in produtos:
+        print(f'Fornecedor: {produto['fornecedor']}')
+
+    fornecedor = input('Digite o fornecedor: ')
+
+    for produto in produtos:
+        if produto['fornecedor'].lower() == fornecedor.lower():
+            print(
+                f'{produto['produto']} - '
+                f'{produto['preco']:.2f}'
+            )
+
+            encontrados = encontrados + 1
+
+    if encontrados == 0: 
+        print('Nenhum produto encontrado!')
+
+
 # funcao para pegar o produto mais caro
 def produto_mais_caro(produtos):
     mais_caro = produtos[0]
@@ -101,13 +147,44 @@ def produto_mais_caro(produtos):
 
     print(f'Produto: {mais_caro['produto']}')
     print(f'Preco: {mais_caro['preco']:.2f}')
-    print(f'Categoria: {mais_caro['categoria']:.2f}')
+    print(f'Categoria: {mais_caro['categoria']}')
 
+# funcoa pegar o produto mais barato
+def produto_mais_barato(produtos):
+    mais_barato = produtos[0]
+
+    for produto in produtos:
+        if produto['preco'] < mais_barato['preco']:
+            mais_barato = produto
+
+    print("\n==== PRODUTO MAIS BARATO É ====\n")
+    
+    print(f'Produto: {mais_barato['produto']}')
+    print(f'Preco: {mais_barato['preco']:.2f}')
+    print(f'Categoria: {mais_barato['categoria']}')
+
+# Calcular o estoque
+# def calcular_estoque(produtos):
+
+
+def menu():
+    print('\n ==== Escolha as opcoes abaixo ====')
+    print('1 - Mostrar Produtos')
+    print('2 - Calcular a media')
+    print('3 - Produtos acima...')
+    print('4 - Produtos abaixo...')
+    print('5 - Produto mais barato')
+    print('6 - Produto mais caro')
+    print('7 - Filtrar por Categoria')
+    print('8 - Filtrar por Fornecedor')
+    print('9 - Calcular estoque')
+    print('10 - Encerrar')
 
 produtos = carregar_produtos()
 
 while True:
-
+    # puxar a funcao de menu
+    menu()
     opcao = int(input('Digite uma opcao: '))
 
     if opcao == 1:
@@ -119,4 +196,12 @@ while True:
     elif opcao == 4:
         produtos_abaixo(produtos)
     elif opcao == 5:
+        produto_mais_barato(produtos)
+    elif opcao == 6:
+        produto_mais_caro(produtos)
+    elif opcao == 7:
+        filtrar_categoria(produtos)
+    elif opcao == 8:
+        filtra_fornecedor(produtos)
+    elif opcao == 9:
         break
